e02d22b8441dcd6405617d076141ed1e9a305929
qulick <58820014+Qulick-k@users.noreply.github.com>
2025-07-11 16:17:50 +0800

    Practic c++ &  GameEngine
    Practic c++ & GameEngine

---
+++ DX3D/Source/DX3D/Graphics/GraphicsLogUtils.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <DX3D/Core/Logger.h>
+
+
+namespace dx3d
+{
+#define DX3DGraphicsLogThrowOnFail(hr, message)\
+	{\
+	auto res = (hr);\
+	if (FAILED(res))\
+		DX3DLogErrorAndThrow(message);\
+	}
+}
